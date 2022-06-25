08287211e9ada70febbfbf3aba8c4d435485e572
brocahontaz <johan@rooter.se>
2022-06-25 17:21:06 +0200

    Removed echo and comment.

--- tools/skaffold/start.sh
+++ tools/skaffold/start.sh
@@ -10,8 +10,6 @@ DIR=$(pwd)
 PROJECT_DIR=${DIR%/*/*}
 APP_DIR=$PROJECT_DIR/demo-app
 
-echo $APP_DIR
-
 k3d cluster create k3d-cluster -p "80:80@loadbalancer" --volume $APP_DIR:/tmp/k3dvol
 
 skaffold run --tail
